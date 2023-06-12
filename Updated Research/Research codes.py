eda852574b749a19d469fe156a52e80a301be939
DSC <sunnyline99800@gmail.com>
2023-06-12 09:27:02 +0200

    Add codes with data loading

---
+++ Updated Research/Research codes.py
@@ -0,0 +1,13 @@
+## Reproducible Research - Spam Detection Project
+
+## Packages Load
+
+import pandas as pd
+import numpy as np
+
+## Data load
+
+data = pd.read_csv("C:/Users/justy/Desktop/Info/Inne/DSC/UW/Semestr IV/RR/RR_Project/Updated Research/spam_ham_dataset.csv")
+data.describe
+
+## Data preprocesing
